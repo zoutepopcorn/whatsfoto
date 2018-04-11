38a32abbc61381025ae85d6140dc3c47e523e5c6
test test <jo@jo>
2018-04-11 17:34:59 +0200

    404 not working

--- content.js
+++ content.js
@@ -9,45 +9,49 @@
 */
 
 const MON = {
-  api : "SECRET",
-  db  : "",
-  col : "",
+  api: "SECRET",
+  db: "",
+  col: "",
 };
 const DB = "https://api.mlab.com/api/1/databases/" + MON.db + "/collections/" + MON.col + "?apiKey=" + MON.api;
 
 
 let req = (url, data) => {
   //console.log(url);
- console.log(" ---");
- console.log(data);
-  $.ajax( {   url: url,
-              data: JSON.stringify( data ),
-              type: "POST",
-              contentType: "application/json" } );
+  console.log(" ---");
+  console.log(data);
+  $.ajax({
+    url: url,
+    data: JSON.stringify(data),
+    type: "POST",
+    contentType: "application/json"
+  });
 }
 
-document.addEventListener('popup',  (e) => {
-
-    let data = e.detail;
-    console.log(">>  "+ JSON.stringify(data));
+document.addEventListener('popup', (e) => {
+  let data = e.detail;
+  console.log(">>  " + JSON.stringify(data));
 
 });
 
 
-document.addEventListener('toDB',  (e) => {
-    console.log("TO_____DB");
-    let data = e.detail;
-    console.log("received "+ JSON.stringify(data));
-    req(DB, data);
+document.addEventListener('toDB', (e) => {
+  console.log("TO_____DB");
+  let data = e.detail;
+  console.log("received " + JSON.stringify(data));
+  req(DB, data);
 });
 
 let injectJs = (link) => {
-    var scr = document.createElement("script");
-    scr.type = "text/javascript";
-    scr.src = link;
-    (document.head || document.body || document.documentElement).appendChild(scr);
+  var scr = document.createElement("script");
+  scr.type = "text/javascript";
+  scr.src = link;
+  (document.head || document.body || document.documentElement).appendChild(scr);
 }
 
 
 injectJs(chrome.extension.getURL("jquery.min.js"));
+injectJs(chrome.extension.getURL("filesaver.js"));
+injectJs(chrome.extension.getURL("jszip.js"));
+injectJs(chrome.extension.getURL("jszip.ut.js"));
 injectJs(chrome.extension.getURL("ui.js"));
--- ui.js
+++ ui.js
@@ -10,97 +10,115 @@
 */
 
 (function() {
-    'use strict';
-    const PREF = "003162186";
-    let begin = 4000;
-    const END = 4030;
-    let pics = 0;
-    const PICS_MAX = 10;   // TODO: tweak, not tested.
+  'use strict';
+  const PREF = "31621863966";
+  let begin = 4000;
+  const END = 4030;
+  let pics = 0;
+  const PICS_MAX = 10; // TODO: tweak, not tested.
+  document.addEventListener('openPic', (e) => {
+    const data = e.detail;
+    console.log(" " + JSON.stringify(data));
+    downAll(data.phone);
+    // picture(data.phone, (out) => {
+    //   console.log("ben ik " + out);
+    //   //chrome.tabs.create({ url: "http://nu.nl" });
+    //   const html = `<div id="overlay">
+    //                           <img src="${out.img}">
+    //                       </div>  `;
+    //
+    //   $("body").append(html);
+    //
+    // });
+  });
 
-    document.addEventListener('openPic',  (e) => {
-        const data = e.detail;
-        console.log("<> "+ JSON.stringify(data));
-        picture(data.phone, (out) => {
-            console.log("ben ik " + out);
-            //chrome.tabs.create({ url: "http://nu.nl" });
-            const html = `<div id="overlay">
-                              <img src="${out.img}">
-                          </div>  `;
+  var evt = document.createEvent("CustomEvent");
 
-            $("body").append(html);
-            //window.open("data:text/html;charset=utf-8," + html, "")
 
-        });
-    });
-
-    var evt = document.createEvent("CustomEvent");
-    let toDB = (json) => {
-        console.log(" JSOOOOOOOOOOOOON   ");
-        console.log(json);
-        console.log("--------------");
-        evt.initCustomEvent("toDB", true, true, json);
-        document.dispatchEvent(evt);
-
-    }
-
-    let picture = (nr, cb) => {
-        console.log("nr: " + nr);
-        pics++;
-        Store.ProfilePicThumb.find( nr + '@c.us').then((d) => {
-            pics--;
-            console.log("----");
-            console.log(d.img);
-            if (d.img === null) {
-                picture(nr);
-            }
-            if (d.img === undefined) {
-                //console.log("not found");
-                cb(false);
-            } else {
-                console.log(d.img);
-                console.log(d.imgFull);
-                console.log(d);
-                cb( {"thumb" : d.img, "img" : d.imgFull } );
-                // toDB({"img" : d.img, "full" : d.imgFull});
-            }
-        }, (e) => {
-            //cb(e);
-            // Server is throttling/rate limiting, we try it again
-            //picture(nr);
-        });
-
-    }
-
-    let start = () => {
-      console.log("start ");
-      //addMongo();
-
-      let scrape = setInterval(() => {
-              const NR = PREF + begin;
-              console.log(NR);
-              if(pics < PICS_MAX) {
-                  picture(NR);
-                  if(begin > END) {
-                    clearInterval(scrape);
-                    console.log("STOP");
-                  } else {
-                    begin++;
-                  }
-              }
-        }, 400);
-    }
-
-    // check if Store is ready, then start scraping
-    let wait = setInterval(() => {
-      console.log($("body"));
-
-      //console.log(Store);
-      if(Store) {
-        clearInterval(wait);
-        // console.clear();
-        // setTimeout(() => start() , 15000);
-      } else {
-        console.log(".. Store waiting ..");
+  const splitNrs = (nrs) => {
+    let output = nrs.split(',')
+    console.log(output);
+    let myArr = [];
+    output.forEach((el) => {
+      el = el.replace(/[^0-9]/, '');
+      if (el.indexOf("06") == 0) {
+        el = el.replace("06", "316");
       }
-    }, 300);
+      if (el.length != 11) {
+
+      } else {
+
+      }
+      myArr.push(el)
+    })
+    return myArr;
+  }
+
+
+  let downAll = (nrs, cb) => {
+    let findAll = [];
+    let zip = new JSZip();
+
+    let urlToPromise = (url) => {
+      return new Promise(function(resolve, reject) {
+        JSZipUtils.getBinaryContent(url, function(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+    }
+
+    splitNrs(nrs).forEach((number) => {
+      console.log(number);
+      findAll.push(Store.ProfilePicThumb.find(`${number}@c.us`));
+    });
+    Promise.all(findAll)
+      .then((out) => {
+        out.forEach((item) => {
+          // item
+          zip.file(`${item.id}`, urlToPromise(item.imgFull), {
+            binary: true
+          });
+        })
+        zip.generateAsync({
+          type: "blob"
+        }).then(function callback(blob) {
+          saveAs(blob, "example.zip");
+        });
+        console.log(out);
+      })
+  }
+
+  let picture = (nr, cb) => {
+    console.log("nr: " + nr);
+    pics++;
+    Store.ProfilePicThumb.find(nr + '@c.us').then((d) => {
+      pics--;
+      console.log("----");
+      console.log(d.img);
+      if (d.img === null) {
+        picture(nr);
+      }
+      if (d.img === undefined) {
+        //console.log("not found");
+        cb(false);
+      } else {
+        console.log(d.img);
+        console.log(d.imgFull);
+        console.log(d);
+        cb({
+          "thumb": d.img,
+          "img": d.imgFull
+        });
+        // toDB({"img" : d.img, "full" : d.imgFull});
+      }
+    }, (e) => {
+      //cb(e);
+      // Server is throttling/rate limiting, we try it again
+      //picture(nr);
+    });
+  }
 })();
